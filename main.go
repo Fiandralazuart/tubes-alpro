63063a05ae5e01daebfa9432d125aee13a7739a7
Fathir <fathirmuhammadarsy1@gmail.com>
2026-05-22 20:47:07 +0700

    feat: update main function update penyewa dan calling back menu agar ada pilihan y/n

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import "fmt"
-
-func runProject() {
-	fmt.Println("Selamat Datang di Tubes Alpro")
-}
-
 func main() {
-	fmt.Println("hello")
-
-	fmt.Println("Ini Project Tubes Alpro 2")
+	menuPenyewa()
 }
